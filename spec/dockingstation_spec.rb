9d62d90180c50928d76aab7214206c0f533db9fa
Jonathan Andrews <jonathanandrews@Jonathans-MacBook-Pro.local>
2018-09-07 14:45:45 +0000

    Used method stubs in Rspec tests

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,47 +2,26 @@ require 'dockingstation'
 require 'bike'
 
 describe DockingStation do
+  let(:double_bike) { double :bike }
   it 'responds to method call release_bike' do
     expect(DockingStation.new()).to respond_to(:release_bike)
   end
 
-  # it 'responds to method call working?' do
-  #   expect(DockingStation.new()).to respond_to(:working?)
-  # end
-  it 'creates a new bike object when calling release_bike' do
-    bike_1 = Bike.new
-    subject.dock_bike(bike_1)
-    expect(subject.release_bike).to be_a_kind_of(Bike)
-  end
-
   it 'checks that the bike is working?' do
-    bike_1 = Bike.new
-    subject.dock_bike(bike_1)
+    allow(double_bike).to receive(:working?).and_return(true)
+    subject.dock_bike(double_bike)
     expect(subject.release_bike.working?).to eq true
   end
 
-  # it 'responds to method called dock_bike' do
-  #   expect(DockingStation.new().dock_bike(Bike.new)).to be_a_kind_of(Array)
-  # end
-
-  # it 'checks each object in the dock_bike array is a bike object' do
-  #   aldgate = DockingStation.new()
-  #   5.times do
-  #     aldgate.dock_bike(Bike.new)
-  #   end
-  #   aldgate.dock_bike(Bike.new).each do |bike|
-  #     expect(bike).to be_a_kind_of(Bike)
-  #   end
-  # end
   it 'tests that random_method returns a NoMethodError' do
     expect {subject.random_method}.to raise_error(NoMethodError)
   end
 
   # describe '#release_bike' do
     it 'releases the bike we dock' do
-      bike_1 = Bike.new
-      subject.dock_bike(bike_1)
-      expect(subject.release_bike).to eq bike_1
+      allow(double_bike).to receive(:working?).and_return(true)
+      subject.dock_bike(double_bike)
+      expect(subject.release_bike).to eq(double_bike)
     end
   # end
   it 'gives us a no bike available error when no bikes are available' do
@@ -50,17 +29,17 @@ describe DockingStation do
   end
 
   it 'errors out if you put in 20 or more bikes' do
-    expect {(DockingStation::DEFAULT_CAPACITY+1).times{subject.dock_bike(Bike.new)}}.to raise_error('too many bikes')
+    expect {(DockingStation::DEFAULT_CAPACITY+1).times{subject.dock_bike(double_bike)}}.to raise_error('too many bikes')
   end
 
   it 'tests the capacity of a dockingstation when we set it to 40' do
     aldgate = DockingStation.new(40)
-    expect {30.times{aldgate.dock_bike(Bike.new)}}.not_to raise_error
+    expect {30.times{aldgate.dock_bike(double_bike)}}.not_to raise_error
   end
 
   it "Can't fit more than 40 bikes on the bike rack" do
     aldgate = DockingStation.new(40)
-    expect {41.times{aldgate.dock_bike(Bike.new)}}.to raise_error('too many bikes')
+    expect {41.times{aldgate.dock_bike(double_bike)}}.to raise_error('too many bikes')
   end
 
   it 'expects default capacity to be 20' do
@@ -72,25 +51,28 @@ describe DockingStation do
   end
 
   it 'method "report_broken_and_dock" stores bike marked as broken' do
-    bike = Bike.new
+    allow(double_bike).to receive(:broken)
+    allow(double_bike).to receive(:working?).and_return(false)
+    bike = double_bike
     subject.report_broken_and_dock(bike)
     expect(bike.working?).to eq(false)
   end
 
   it "raises error 'Sorry, all the bikes are broken'" do
-    bike = Bike.new
+    allow(double_bike).to receive(:broken)
+    allow(double_bike).to receive(:working?).and_return(false)
+    bike = double_bike
     subject.report_broken_and_dock(bike)
     expect {subject.release_bike}.to raise_error('Sorry, all the bikes are broken')
   end
 
-  let(:bike) { double :bike }
   it "release unbroken bikes only" do
-    allow(bike).to receive(:working?).and_return(true)
-    allow(bike).to receive(:broken).and_return(true)
-    good_bike = bike
+    allow(double_bike).to receive(:working?).and_return(true)
+    allow(double_bike).to receive(:broken).and_return(true)
+    good_bike = double_bike
     subject.dock_bike(good_bike)
     5.times do
-      subject.report_broken_and_dock(bike)
+      subject.report_broken_and_dock(double_bike)
     end
     expect(subject.release_bike).to eq(good_bike)
   end
